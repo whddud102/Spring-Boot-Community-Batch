a5876f5ad37efab16ff8e3e079b737b0c1eb610a
JongYoung <whddud102@naver.com>
2020-05-13 17:05:36 +0900

    오타 수정
    BatchApplicationTests.java
    - import 경로 수정

--- src/test/java/com/community/batch/BatchApplicationTests.java
+++ src/test/java/com/community/batch/BatchApplicationTests.java
@@ -1,7 +1,8 @@
 package com.community.batch;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class BatchApplicationTests {
