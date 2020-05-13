e1b102ace87b51799aac49454787334fbfe48a15
JongYoung <whddud102@naver.com>
2020-05-13 21:20:24 +0900

    리팩토링
    InactiveUserJobTest.java
    - 테스트 코드 수행 시, H2를 DB로 사용하도록 설정

--- src/test/java/com/community/batch/InactiveUserJobTest.java
+++ src/test/java/com/community/batch/InactiveUserJobTest.java
@@ -8,6 +8,8 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,18 +22,19 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class InactiveUserJobTest {
-    @Autowired
-    private JobLauncherTestUtils jobLauncherTestUtils;
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Test
-    public void test_InactiveUserStatus() throws Exception {
-        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+	@Test
+	public void test_InactiveUserStatus() throws Exception {
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
 
-        assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());  // 배치 실행 결과 확인
-        assertEquals(0, userRepository.findByUpdatedBeforeAndStatusEquls(LocalDateTime.now().minusYears(1), UserStatus.ACTIVE).size());
-    }
+		assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());  // 배치 실행 결과 확인
+		assertEquals(0, userRepository.findByUpdatedDateBeforeAndStatusEquals(LocalDateTime.now().minusYears(1), UserStatus.ACTIVE).size());
+	}
 }
